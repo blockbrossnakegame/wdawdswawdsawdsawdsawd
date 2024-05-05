cc02588787b324bdb5cf5a4d61e00c7c1bd507ad
blockbrossnakegame <159715270+blockbrossnakegame@users.noreply.github.com>
2024-05-05 13:59:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ let Discord = require("discord.js");
 let client = new Discord.Client();
 let { MessageEmbed } = require("discord.js");
 const express = require("express");
-const keep_alive = require('./keep_alive.js');
 const app = express();
 const allowedRole = '1208186017337581699';
 const owner = '1115992837775953951';
@@ -173,7 +172,7 @@ client.on("message", async (message) => {
         const embed = new MessageEmbed()
         .setTitle(`**Help**`)
         .setDescription(`Tbh idk what to write here but here are some commands u can use.
-      
+
 **!help** - the command u just used
 **!8ball** - answers your questions`)
         .setColor("#90EE90");
